10b6f9e96891c73115f161f8009012015d5dfbe9
GeneCannella <gene.cannella@gmail.com>
2018-02-23 22:59:25 -0500

    adding schema

---
+++ starterData.sql
@@ -0,0 +1,7 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Memory Stick", "Electronics", 2.50, 100);
+VALUES ("Memory Stick", "Electronics", 2.50, 100), 
+("War and Peace", "Books", 9.95, 20), 
+("Foundation", "Books", 9.95, 5);
